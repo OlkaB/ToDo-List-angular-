5dcf28e763e989440027f54becebfba812b2388c
Aleksandra <o.barecka@gmail.com>
2017-01-21 17:19:20 +0100

    Further enchancements; to do: delete task + task list sorting

--- js.js
+++ js.js
@@ -2,24 +2,30 @@ var app = angular.module("myApp", []);
 
 //days to drop-down list
 app.controller("tasks", function($scope) {
-$scope.days=[];
+  $scope.days=[];
   $scope.days = ["monday", "teusday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 
 //store task item to taskList object
-  $scope.taskList = [];
-  $scope.taskId = 0;
-  $scope.addTask = function() {
-    $scope.taskId++;
-    $scope.task = "";
-    $scope.day = "";
-    $scope.item = {
-      id: $scope.taskId,
-      task: $scope.task,
-      deadline: $scope.day
-    }
-    $scope.taskList.push($scope.item);
-    console.log($scope.taskList);
-  };
+$scope.taskList = [];
+$scope.taskId = 0;
+$scope.addTask = function() {
+  $scope.taskId++;
+  $scope.item = {
+    id: $scope.taskId,
+    task: $scope.task,
+    deadline: $scope.deadline
+  }
+  $scope.taskList.push($scope.item);
+  
+  //clearing inputs
+  $scope.task = "";
+  $scope.deadline = "";
+
+  //deleting task from Task list
+  $scope.deleteTask = function() {
+    
+  }
+};
 
 
 
