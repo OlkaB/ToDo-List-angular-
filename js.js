908199713d2fc3da4b199caffaa426f69a3fc8d6
Aleksandra <o.barecka@gmail.com>
2017-01-21 11:19:11 +0100

    first upload: done {create object and add task to list}, to be done {clear fields after adding task + option to dele task from list}

---
+++ js.js
@@ -0,0 +1,24 @@
+var app = angular.module("myApp", []);
+
+//days to drop-down list
+app.controller("tasks", function($scope) {
+$scope.days=[];
+  $scope.days = ["monday", "teusday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+
+//store task item to taskList object
+  $scope.taskList = [];
+  $scope.taskId = 0;
+  $scope.addTask = function() {
+    $scope.taskId++;
+    $scope.item = {
+      id: $scope.taskId,
+      task: $scope.task,
+      deadline: $scope.day
+    }
+    $scope.taskList.push($scope.item);
+    console.log($scope.taskList);
+  };
+
+
+
+});
